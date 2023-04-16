23b51588df4677da4be6047f08acd145ce81da88
Stephan Lipp <sphlpp91@gmail.com>
2023-04-17 00:14:10 +0200

    [SA][SAST] Added SAST tool runner

---
+++ src/static_analysis/sast/sfa/logic/runner.py
@@ -0,0 +1,35 @@
+from itertools import chain
+from multiprocessing import Pool
+from typing import List
+
+from sfa.logic.tools.base import SASTTool, SASTToolOutput
+
+
+def _starter(tool: SASTTool) -> SASTToolOutput:
+    """Run a single SAST tool.
+
+    :param tool: SAST tool runner
+    :return: SAST tool output
+    """
+    return tool.run()
+
+
+def run_sast_tools(tools: List[SASTTool], run_parallel: bool = True) -> SASTToolOutput:
+    """Run multiple SAST tools.
+
+    Given a list of SAST tools, this function executes the corresponding runners in parallel resp. sequence, followed by
+    merging the tools' output into a single result set.
+
+    :param tools: SAST tool runners
+    :param run_parallel: Parallel runner exec.
+    :return: Output of SAST tools
+    """
+    assert len(tools) > 0
+
+    if not run_parallel:
+        temp_res = list(map(lambda tool: tool.run(), tools))
+    else:
+        with Pool(len(tools)) as pool:
+            temp_res = pool.map(_starter, tools)
+
+    return set(chain(*temp_res))
