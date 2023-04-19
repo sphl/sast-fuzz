b072e2685d7b8b3934cb6443fafa432c6a38634a
Stephan Lipp <sphlpp91@gmail.com>
2023-04-20 00:37:20 +0200

    [SA][SAST] Added required program arguments

--- src/static_analysis/sast/sfa/main.py
+++ src/static_analysis/sast/sfa/main.py
@@ -1,24 +1,29 @@
 import argparse
 from os import path
 
-from sfa.logic.tools.factory import SASTTool, SASTToolFactory
 from sfa.logic.runner import run_sast_tools
+from sfa.logic.tools.factory import SASTTool, SASTToolFactory
 
 
 def sfa() -> int:
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-i", "--subject-dir", metavar="<dir-path>", dest="subject_dir", type=str, required=True,
+    parser.add_argument("-i", "--subject-dir", metavar="<path>", dest="subject_dir", type=str, required=True,
                         help="Directory path to the program to be analyzed.")
+    parser.add_argument("-o", "--output-file", metavar="<path>", dest="output_file", type=str, required=False,
+                        help="Path to the output CSV file.")
+    parser.add_argument("-x", "--exclude-tool", choices=SASTTool.values(), action="append", dest="excluded_tools",
+                        default=list(), required=False, help="SAST tool(s) to be excluded from the analysis.")
 
     args = parser.parse_args()
 
     subject_dir = path.expanduser(args.subject_dir)
+    output_file = path.expanduser(args.output_file)
 
     assert path.exists(subject_dir)
 
     factory = SASTToolFactory(subject_dir)
-    runners = list(map(factory.get_runner, [tool for tool in SASTTool]))
+    runners = list(map(factory.get_runner, [tool for tool in SASTTool.values() if tool not in args.excluded_tools]))
 
     findings = run_sast_tools(runners)
 
