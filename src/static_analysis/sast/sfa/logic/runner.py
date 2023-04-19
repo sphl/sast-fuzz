2e1140b80d26f4c873daae8de53964b36e7f8567
Stephan Lipp <sphlpp91@gmail.com>
2023-04-20 00:08:36 +0200

    [SA][SAST] Updated doc

--- src/static_analysis/sast/sfa/logic/runner.py
+++ src/static_analysis/sast/sfa/logic/runner.py
@@ -17,8 +17,8 @@ def _starter(tool: SASTToolRunner) -> SASTToolOutput:
 def run_sast_tools(runners: List[SASTToolRunner], exec_parallel: bool = True) -> SASTToolOutput:
     """Run multiple SAST tools.
 
-    Given a list of SAST tools, this function executes the corresponding runners in parallel resp. sequence, followed by
-    merging the tools' output into a single result set.
+    This function executes the passed SAST tool runners in parallel resp. sequence, followed by merging the tools'
+    output into a single result set.
 
     :param runners: SAST tool runners
     :param exec_parallel: Parallel runner execution
