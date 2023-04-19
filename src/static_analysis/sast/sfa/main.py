2b33de5d2f52b76ea1c0e641902d284792a0b793
Stephan Lipp <sphlpp91@gmail.com>
2023-04-20 00:46:56 +0200

    [SA][SAST] Fix

--- src/static_analysis/sast/sfa/main.py
+++ src/static_analysis/sast/sfa/main.py
@@ -10,7 +10,7 @@ def sfa() -> int:
 
     parser.add_argument("-i", "--subject-dir", metavar="<path>", dest="subject_dir", type=str, required=True,
                         help="Directory path to the program to be analyzed.")
-    parser.add_argument("-o", "--output-file", metavar="<path>", dest="output_file", type=str, required=False,
+    parser.add_argument("-o", "--output-file", metavar="<path>", dest="output_file", type=str, required=True,
                         help="Path to the output CSV file.")
     parser.add_argument("-x", "--exclude-tool", choices=SASTTool.values(), action="append", dest="excluded_tools",
                         default=list(), required=False, help="SAST tool(s) to be excluded from the analysis.")
@@ -20,7 +20,8 @@ def sfa() -> int:
     subject_dir = path.expanduser(args.subject_dir)
     output_file = path.expanduser(args.output_file)
 
-    assert path.exists(subject_dir)
+    assert path.exists(subject_dir), "ERROR: Subject directory does not exist!"
+    assert path.exists(path.dirname(output_file)), "ERROR: ..."
 
     factory = SASTToolFactory(subject_dir)
     runners = list(map(factory.get_runner, [tool for tool in SASTTool.values() if tool not in args.excluded_tools]))
