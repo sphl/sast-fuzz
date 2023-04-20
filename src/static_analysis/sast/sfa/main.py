e50934f088d7e1849daf52f6cd149c0227ae889c
Stephan Lipp <sphlpp91@gmail.com>
2023-04-20 11:36:00 +0200

    [SA][SAST] Fix

--- src/static_analysis/sast/sfa/main.py
+++ src/static_analysis/sast/sfa/main.py
@@ -20,11 +20,14 @@ def sfa() -> int:
     subject_dir = path.expanduser(args.subject_dir)
     output_file = path.expanduser(args.output_file)
 
+    selected_tools = [tool for tool in SASTTool.values() if tool not in args.excluded_tools]
+
     assert path.exists(subject_dir), "ERROR: Subject directory does not exist!"
-    assert path.exists(path.dirname(output_file)), "ERROR: ..."
+    assert path.exists(path.dirname(output_file)), "ERROR: Directory of output file does not exist!"
+    assert len(selected_tools) >= 1, "ERROR: All SAST tools have been disabled!"
 
     factory = SASTToolFactory(subject_dir)
-    runners = list(map(factory.get_runner, [tool for tool in SASTTool.values() if tool not in args.excluded_tools]))
+    runners = list(map(factory.get_runner, selected_tools))
 
     findings = run_sast_tools(runners)
 
