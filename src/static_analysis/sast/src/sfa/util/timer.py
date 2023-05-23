88e65e8e7aad54d0d401334975476dd82d576d55
Stephan Lipp <sphlpp91@gmail.com>
2023-05-23 15:37:50 +0200

    [SA][SAST] Added timer to measure execution time

---
+++ src/static_analysis/sast/src/sfa/util/timer.py
@@ -0,0 +1,16 @@
+import time
+from typing import Any, Tuple, Callable
+
+
+def get_exec_time(func: Callable) -> Tuple[Any, float]:
+    """
+    Return the execution time of a given function.
+
+    :param func:
+    :return:
+    """
+    t1 = time.time()
+    results = func()
+    t2 = time.time()
+
+    return results, (t2 - t1)
