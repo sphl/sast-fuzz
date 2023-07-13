db77ad38b2a31199c5d6e142c25fb8c663570496
Stephan Lipp <sphlpp91@gmail.com>
2023-07-13 13:27:59 +0200

    feat(fuzzing): add linear fuzz input rate (testing)

--- src/afl-fuzz.c
+++ src/afl-fuzz.c
@@ -514,9 +514,12 @@ void update_cycle_length_fix() {
     cycle_length = init_cycle_length;
 }
 
-void update_cycle_length_log(uint16_t duration) {
-    uint16_t h = duration / 60;
-    cycle_length = (uint64_t)(log2(h + 1) * 1000) + init_cycle_length;
+void update_cycle_length_lin(uint32_t inc) {
+    cycle_length = (cycle_length + inc);
+}
+
+void update_cycle_length_log(uint32_t dur) {
+    cycle_length = (uint64_t)(log2((dur / 60) + 1) * 1000) + init_cycle_length;
 }
 // ---------------------------------------------------------------------------------------------------------------------
 static u32 num_critical_bbs;
