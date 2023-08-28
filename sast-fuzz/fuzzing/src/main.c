dfd2173bb3cc0adf1f7d7edd98bad03dbcb74e99
Stephan Lipp <sphlpp91@gmail.com>
2023-08-28 16:31:20 +0200

    refactor(fuzzing): re-order functions (2)

--- sast-fuzz/fuzzing/src/main.c
+++ sast-fuzz/fuzzing/src/main.c
@@ -392,166 +392,6 @@ static int32_t **distance_matrix = NULL;
 static float *cbb_distances = NULL;
 // =====================================================================================================================
 
-void update_tbb_states() {
-    float sum_vuln_score = 0.0f;
-
-    for (u32 i = 0; i < n_tbbs; i++) {
-        if (tbb_infos[i]->state == active || tbb_infos[i]->state == paused) {
-            sum_vuln_score += tbb_infos[i]->vuln_score;
-        }
-    }
-
-    u32 n_tbbs_paused = 0;
-    u32 n_tbbs_finished = 0;
-
-    for (u32 i = 0; i < n_tbbs; i++) {
-        if (tbb_infos[i]->state == active || tbb_infos[i]->state == paused) {
-
-            int64_t n_req_input_execs =
-                    (int64_t)roundf((float)cycle_length * (tbb_infos[i]->vuln_score / sum_vuln_score));
-
-            if (hc_reduct_factor == 1.0f) {
-                n_req_input_execs = 1;
-            } else {
-                n_req_input_execs -= (int64_t)((float)n_req_input_execs * hc_reduct_factor);
-            }
-
-            int64_t exec_diff = (n_req_input_execs - (int64_t)tbb_infos[i]->n_input_execs);
-
-            if (exec_diff <= 0) {
-
-                // We do not care if the target BB is activated or paused. When it has been executed frequently enough
-                // by the generated fuzzy inputs, we mark it as finished
-                tbb_infos[i]->state = finished;
-
-            } else {
-
-                if (tbb_infos[i]->cov_flag) {
-
-                    // Each executed target BB will automatically be activated in the next cycle, regardless if paused
-                    // or already activated
-                    tbb_infos[i]->state = active;
-                    tbb_infos[i]->n_cycle_skips = 0;
-                    tbb_infos[i]->n_prev_cycle_skips = 1;
-
-                } else {
-
-                    if (tbb_infos[i]->n_cycle_skips == 0) {
-
-                        tbb_infos[i]->state = paused;
-                        tbb_infos[i]->n_cycle_skips = tbb_infos[i]->n_prev_cycle_skips;
-                        tbb_infos[i]->n_prev_cycle_skips++;
-
-                    } else {
-
-                        // Reactivate target BB if it has been "sufficiently" paused
-                        if ((tbb_infos[i]->n_cycle_skips - 1) == 0) {
-                            tbb_infos[i]->state = active;
-                            tbb_infos[i]->n_cycle_skips = 0;
-                        } else {
-                            tbb_infos[i]->n_cycle_skips--;
-                        }
-                    }
-                }
-            }
-
-#ifdef SFZ_DEBUG
-            char status_str[128];
-
-            if (tbb_infos[i]->state == finished) {
-                sprintf(status_str, "x");
-            } else {
-                if (tbb_infos[i]->state == active) {
-                    sprintf(status_str, "...");
-                } else {
-                    sprintf(status_str, "p (%llu|%llu)", tbb_infos[i]->n_cycle_skips, tbb_infos[i]->n_prev_cycle_skips);
-                }
-            }
-
-            printf("sast-fuzz: target BB = %d (%.2f), required = %ld (%.1f), actual = %llu (%ld) %s\n", i,
-                   tbb_infos[i]->vuln_score, n_req_input_execs, hc_reduct_factor, tbb_infos[i]->n_input_execs,
-                   exec_diff, status_str);
-#endif
-
-            // Reset coverage flag
-            tbb_infos[i]->cov_flag = false;
-        }
-
-        if (tbb_infos[i]->state == paused) {
-            n_tbbs_paused++;
-        }
-
-        if (tbb_infos[i]->state == finished) {
-            n_tbbs_finished++;
-        }
-    }
-
-#ifdef SFZ_DEBUG
-    printf("sast-fuzz: target BBs finished = %d, active = %d, paused = %d\n", n_tbbs_finished,
-           (n_tbbs - (n_tbbs_finished + n_tbbs_paused)), n_tbbs_paused);
-#endif
-
-    u32 n_cbbs_solved = 0;
-    u32 n_cbbs_hc_exceeded = 0;
-
-    assert(n_cbbs == critical_ids[0]);
-
-    for (u32 i = 0; i < n_cbbs; i++) {
-        u32 cbb_id = critical_ids[i + 1];
-
-        if (solved_cbbs[cbb_id]) {
-            n_cbbs_solved++;
-        }
-
-        if (critical_count[cbb_id] > BLOCK_TIMES) {
-            n_cbbs_hc_exceeded++;
-        }
-    }
-
-    // Determine fuzzing strategy for the next cycle
-    if (n_cbbs_solved == n_cbbs) {
-
-        if (n_tbbs_finished == n_tbbs) {
-
-            memset(solved_cbbs, 0, MAP_SIZE);
-            memset(critical_count, 0, MAP_SIZE * sizeof(u32));
-
-            for (int i = 0; i < n_tbbs; i++) {
-                // Reactivate all target BBs with a vulnerability score of at least 'vuln_score_thres'
-                if (tbb_infos[i]->vuln_score >= vuln_score_thres) {
-                    tbb_infos[i]->state = active;
-                    tbb_infos[i]->cov_flag = false;
-                    tbb_infos[i]->n_input_execs = 0;
-                    tbb_infos[i]->n_cycle_skips = 0;
-                    tbb_infos[i]->n_prev_cycle_skips = 1;
-                }
-            }
-
-        } else {
-
-            if ((n_tbbs_finished + n_tbbs_paused) == n_tbbs) {
-                explore_status = true;   // coverage mode
-            } else {
-                explore_status = false;  // directed mode
-            }
-        }
-
-    } else {
-
-        assert(n_tbbs_finished != n_tbbs);
-
-        if (n_cbbs_hc_exceeded == n_cbbs) {
-            explore_status = true;  // coverage mode
-        } else {
-            if ((n_tbbs_finished + n_tbbs_paused) == n_tbbs) {
-                explore_status = true;   // coverage mode
-            } else {
-                explore_status = false;  // directed mode
-            }
-        }
-    }
-}
-
 /* Fuzzing stages */
 
 enum {
@@ -5996,6 +5836,166 @@ abort_trimming:
 
     bytes_trim_out += q->len;
     return fault;
+}
+
+void update_tbb_states() {
+    float sum_vuln_score = 0.0f;
+
+    for (u32 i = 0; i < n_tbbs; i++) {
+        if (tbb_infos[i]->state == active || tbb_infos[i]->state == paused) {
+            sum_vuln_score += tbb_infos[i]->vuln_score;
+        }
+    }
+
+    u32 n_tbbs_paused = 0;
+    u32 n_tbbs_finished = 0;
+
+    for (u32 i = 0; i < n_tbbs; i++) {
+        if (tbb_infos[i]->state == active || tbb_infos[i]->state == paused) {
+
+            int64_t n_req_input_execs =
+                    (int64_t)roundf((float)cycle_length * (tbb_infos[i]->vuln_score / sum_vuln_score));
+
+            if (hc_reduct_factor == 1.0f) {
+                n_req_input_execs = 1;
+            } else {
+                n_req_input_execs -= (int64_t)((float)n_req_input_execs * hc_reduct_factor);
+            }
+
+            int64_t exec_diff = (n_req_input_execs - (int64_t)tbb_infos[i]->n_input_execs);
+
+            if (exec_diff <= 0) {
+
+                // We do not care if the target BB is activated or paused. When it has been executed frequently enough
+                // by the generated fuzzy inputs, we mark it as finished
+                tbb_infos[i]->state = finished;
+
+            } else {
+
+                if (tbb_infos[i]->cov_flag) {
+
+                    // Each executed target BB will automatically be activated in the next cycle, regardless if paused
+                    // or already activated
+                    tbb_infos[i]->state = active;
+                    tbb_infos[i]->n_cycle_skips = 0;
+                    tbb_infos[i]->n_prev_cycle_skips = 1;
+
+                } else {
+
+                    if (tbb_infos[i]->n_cycle_skips == 0) {
+
+                        tbb_infos[i]->state = paused;
+                        tbb_infos[i]->n_cycle_skips = tbb_infos[i]->n_prev_cycle_skips;
+                        tbb_infos[i]->n_prev_cycle_skips++;
+
+                    } else {
+
+                        // Reactivate target BB if it has been "sufficiently" paused
+                        if ((tbb_infos[i]->n_cycle_skips - 1) == 0) {
+                            tbb_infos[i]->state = active;
+                            tbb_infos[i]->n_cycle_skips = 0;
+                        } else {
+                            tbb_infos[i]->n_cycle_skips--;
+                        }
+                    }
+                }
+            }
+
+#ifdef SFZ_DEBUG
+            char status_str[128];
+
+            if (tbb_infos[i]->state == finished) {
+                sprintf(status_str, "x");
+            } else {
+                if (tbb_infos[i]->state == active) {
+                    sprintf(status_str, "...");
+                } else {
+                    sprintf(status_str, "p (%llu|%llu)", tbb_infos[i]->n_cycle_skips, tbb_infos[i]->n_prev_cycle_skips);
+                }
+            }
+
+            printf("sast-fuzz: target BB = %d (%.2f), required = %ld (%.1f), actual = %llu (%ld) %s\n", i,
+                   tbb_infos[i]->vuln_score, n_req_input_execs, hc_reduct_factor, tbb_infos[i]->n_input_execs,
+                   exec_diff, status_str);
+#endif
+
+            // Reset coverage flag
+            tbb_infos[i]->cov_flag = false;
+        }
+
+        if (tbb_infos[i]->state == paused) {
+            n_tbbs_paused++;
+        }
+
+        if (tbb_infos[i]->state == finished) {
+            n_tbbs_finished++;
+        }
+    }
+
+#ifdef SFZ_DEBUG
+    printf("sast-fuzz: target BBs finished = %d, active = %d, paused = %d\n", n_tbbs_finished,
+           (n_tbbs - (n_tbbs_finished + n_tbbs_paused)), n_tbbs_paused);
+#endif
+
+    u32 n_cbbs_solved = 0;
+    u32 n_cbbs_hc_exceeded = 0;
+
+    assert(n_cbbs == critical_ids[0]);
+
+    for (u32 i = 0; i < n_cbbs; i++) {
+        u32 cbb_id = critical_ids[i + 1];
+
+        if (solved_cbbs[cbb_id]) {
+            n_cbbs_solved++;
+        }
+
+        if (critical_count[cbb_id] > BLOCK_TIMES) {
+            n_cbbs_hc_exceeded++;
+        }
+    }
+
+    // Determine fuzzing strategy for the next cycle
+    if (n_cbbs_solved == n_cbbs) {
+
+        if (n_tbbs_finished == n_tbbs) {
+
+            memset(solved_cbbs, 0, MAP_SIZE);
+            memset(critical_count, 0, MAP_SIZE * sizeof(u32));
+
+            for (int i = 0; i < n_tbbs; i++) {
+                // Reactivate all target BBs with a vulnerability score of at least 'vuln_score_thres'
+                if (tbb_infos[i]->vuln_score >= vuln_score_thres) {
+                    tbb_infos[i]->state = active;
+                    tbb_infos[i]->cov_flag = false;
+                    tbb_infos[i]->n_input_execs = 0;
+                    tbb_infos[i]->n_cycle_skips = 0;
+                    tbb_infos[i]->n_prev_cycle_skips = 1;
+                }
+            }
+
+        } else {
+
+            if ((n_tbbs_finished + n_tbbs_paused) == n_tbbs) {
+                explore_status = true;   // coverage mode
+            } else {
+                explore_status = false;  // directed mode
+            }
+        }
+
+    } else {
+
+        assert(n_tbbs_finished != n_tbbs);
+
+        if (n_cbbs_hc_exceeded == n_cbbs) {
+            explore_status = true;  // coverage mode
+        } else {
+            if ((n_tbbs_finished + n_tbbs_paused) == n_tbbs) {
+                explore_status = true;   // coverage mode
+            } else {
+                explore_status = false;  // directed mode
+            }
+        }
+    }
 }
 
 /* Write a modified test case, run program, process results. Handle
