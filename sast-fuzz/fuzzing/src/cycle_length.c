7f4a18106817b0ba3b498623ca66ff1266b213cc
Stephan Lipp <sphlpp91@gmail.com>
2023-08-31 23:22:57 +0200

    feat(fuzzing): change constant for log2 cycle interval increase

--- sast-fuzz/fuzzing/src/cycle_length.c
+++ sast-fuzz/fuzzing/src/cycle_length.c
@@ -5,5 +5,5 @@
 u32 lin_cycle_interval(u32 init_interval, u32 increase) { return (init_interval + increase); }
 
 u32 log_cycle_interval(u32 init_interval, u32 duration) {
-    return (init_interval + (u32)(log2f(((float)duration / 3600.0f) + 1.0f) * 300.0f));
+    return (init_interval + (u32)(log2f(((float)duration / 3600.0f) + 1.0f) * 150.0f));
 }
