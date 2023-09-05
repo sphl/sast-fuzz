e924d5ba573dc178c658f09fab998c1386d6a330
Stephan Lipp <sphlpp91@gmail.com>
2023-09-05 11:37:59 +0200

    feat(crash-dedup): parse sanitizer output from file

---
+++ scripts/crash-dedup/src/cdd/container/san.py
@@ -0,0 +1,98 @@
+import re
+from collections import namedtuple
+from enum import Enum, auto
+from pathlib import Path
+from typing import List, Optional
+
+# Stack frame information
+StackFrame = namedtuple("StackFrame", ["id", "file", "function", "line"])
+
+# Stack trace, i.e. list of stack frames
+StackTrace = List[StackFrame]
+
+
+def find_vtype(line: str) -> Optional[str]:
+    """
+    Find the vuln.-type in a sanitizer output line.
+    :param line:
+    :return:
+    """
+    if m := re.search(r"[Ee][Rr][Rr][Oo][Rr]:\s[^:]+:\s([a-zA-Z-_]+)", line):
+        return str(m.group(1)).lower()
+    else:
+        return None
+
+
+def find_frame(line: str) -> Optional[StackFrame]:
+    """
+    Find the stack frame information in a sanitizer output line.
+
+    :param line:
+    :return:
+    """
+    if m := re.search(r"#([0-9]+).*in\s([a-zA-Z0-9_]+)\s([^:]+):([0-9]+)", line):
+        return StackFrame(int(m.group(1)), Path(m.group(3)).name, m.group(2), int(m.group(4)))
+
+    if m := re.search(r"#([0-9]+).*in\s([a-zA-Z0-9_]+)", line):
+        return StackFrame(int(m.group(1)), "-", m.group(2), -1)
+    else:
+        return None
+
+
+class ParseState(Enum):
+    """
+    Sanitizer output parse state.
+    """
+
+    VTYPE = auto()
+    FRAME = auto()
+    TRACE = auto()
+    VALID = auto()
+
+
+class SanitizerOutput:
+    """
+    Sanitizer output container.
+    """
+
+    def __init__(self, vtype: str, stack_trace: StackTrace) -> None:
+        self._vtype = vtype
+        self._stack_trace = stack_trace
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, SanitizerOutput):
+            return False
+
+        return self._vtype == o._vtype and self._stack_trace == o._stack_trace
+
+    @classmethod
+    def from_file(cls, file: Path) -> "SanitizerOutput":
+        vtype = "-"
+        stack_trace = []
+
+        state = ParseState.VTYPE
+
+        for line in [l.strip() for l in file.read_text().splitlines()]:
+            if state == ParseState.VTYPE:
+                if v := find_vtype(line):
+                    vtype = v
+                    state = ParseState.FRAME
+
+            elif state == ParseState.FRAME:
+                if f := find_frame(line):
+                    stack_trace.append(f)
+                    state = ParseState.TRACE
+
+            elif state == ParseState.TRACE:
+                if f := find_frame(line):
+                    stack_trace.append(f)
+                elif len(line) == 0:
+                    state = ParseState.VALID
+                    break
+                else:
+                    break
+
+        if state != ParseState.VALID:
+            raise Exception(f"Invalid sanitizer output in '{file}'!")
+
+        return SanitizerOutput(vtype, stack_trace)
---
+++ scripts/crash-dedup/tests/test_san.py
@@ -0,0 +1,69 @@
+import unittest
+
+from pathlib import Path
+from cdd.container.san import StackFrame, SanitizerOutput
+
+
+class TestSanitizerOutput(unittest.TestCase):
+    def test_from_file(self) -> None:
+        # Arrange
+        test_files = [
+            Path(__file__).parent / "data" / "crashes" / "test.703472",
+            Path(__file__).parent / "data" / "crashes" / "test.703478",
+            Path(__file__).parent / "data" / "crashes" / "test.703498",
+        ]
+
+        expected = [
+            SanitizerOutput(
+                "segv",
+                [
+                    StackFrame(0, "outputscript.c", "outputSWF_TEXT_RECORD", 1429),
+                    StackFrame(1, "outputscript.c", "outputSWF_DEFINETEXT", 1471),
+                    StackFrame(2, "outputscript.c", "outputBlock", 2079),
+                    StackFrame(3, "main.c", "readMovie", 277),
+                    StackFrame(4, "main.c", "main", 350),
+                    StackFrame(5, "libc-start.c", "__libc_start_main", 308),
+                    StackFrame(6, "-", "_start", -1),
+                ],
+            ),
+            SanitizerOutput(
+                "segv",
+                [
+                    StackFrame(0, "decompile.c", "OpCode", 868),
+                    StackFrame(1, "decompile.c", "decompileINCR_DECR", 1474),
+                    StackFrame(2, "decompile.c", "decompileAction", 3225),
+                    StackFrame(3, "decompile.c", "decompileActions", 3401),
+                    StackFrame(4, "decompile.c", "decompile5Action", 3423),
+                    StackFrame(5, "outputscript.c", "outputSWF_DOACTION", 1548),
+                    StackFrame(6, "outputscript.c", "outputBlock", 2079),
+                    StackFrame(7, "main.c", "readMovie", 277),
+                    StackFrame(8, "main.c", "main", 350),
+                    StackFrame(9, "libc-start.c", "__libc_start_main", 308),
+                    StackFrame(10, "-", "_start", -1),
+                ],
+            ),
+            SanitizerOutput(
+                "heap-buffer-overflow",
+                [
+                    StackFrame(0, "asan_interceptors.cpp", "strcat", 375),
+                    StackFrame(1, "decompile.c", "dcputs", 104),
+                    StackFrame(2, "decompile.c", "decompileIMPLEMENTS", 3094),
+                    StackFrame(3, "decompile.c", "decompileAction", 3375),
+                    StackFrame(4, "decompile.c", "decompileActions", 3401),
+                    StackFrame(5, "decompile.c", "decompile5Action", 3423),
+                    StackFrame(6, "outputscript.c", "outputSWF_DOACTION", 1548),
+                    StackFrame(7, "outputscript.c", "outputBlock", 2079),
+                    StackFrame(8, "main.c", "readMovie", 277),
+                    StackFrame(9, "main.c", "main", 350),
+                    StackFrame(10, "libc-start.c", "__libc_start_main", 308),
+                    StackFrame(11, "-", "_start", -1),
+                ],
+            ),
+        ]
+
+        # Act
+        actual = [SanitizerOutput.from_file(file) for file in test_files]
+
+        # Assert
+        for i in range(len(expected)):
+            self.assertEqual(expected[i], actual[i])
