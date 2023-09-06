c3811f58cfe7ab5077e8426986d788176051cb97
Stephan Lipp <sphlpp91@gmail.com>
2023-09-06 16:53:56 +0200

    feat(crash-dedup): allow to specify black-list when searching for files

--- scripts/crash-dedup/src/cdd/utils/fs.py
+++ scripts/crash-dedup/src/cdd/utils/fs.py
@@ -3,20 +3,27 @@ from pathlib import Path
 from typing import List, Optional, Set
 
 
-def find_files(root_dirs: List[Path], exts: Optional[List[str]] = None, rec: bool = False) -> Set[Path]:
+def find_files(
+    root_dirs: List[Path], exts: Optional[List[str]] = None, rec: bool = False, blacklist: Optional[List[str]] = None
+) -> Set[Path]:
     """
     Search for files in various directories.
 
     :param root_dirs: List of directories to start the search from
     :param exts: Allowed file extension
     :param rec: If true, perform recursive search, otherwise, only at top-level
+    :param blacklist: List of filenames to be excluded from the result
     :return: Set of files
     """
+
+    def include(f: Path) -> bool:
+        return (exts is None or f.suffix in exts) and (blacklist is None or not f.name in blacklist)
+
     files: Set[Path] = set()
 
     for root_dir in root_dirs:
         for root, _, _files in walk(root_dir):
-            files.update([Path(root) / Path(file) for file in _files if (exts is None or Path(file).suffix in exts)])
+            files.update([Path(root) / Path(file) for file in _files if include(Path(file))])
 
             if not rec:
                 break
