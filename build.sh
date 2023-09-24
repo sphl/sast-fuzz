5100d7faf624630c3a1cdc902f85a3452474a173
Stephan Lipp <sphlpp91@gmail.com>
2023-09-24 07:50:24 +0200

    refactor: remove old docker files

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+build_type=${1:-Release}
+
 [ -d build ] && rm -rf build
 
 mkdir build
 
 pushd build
-cmake -DCMAKE_BUILD_TYPE=${1:-Release} .. && make -j
+cmake -DCMAKE_BUILD_TYPE=$build_type .. && make -j
 popd
+
+echo "INFO: ${build_type}-build finished!"
