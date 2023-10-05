1f32f84b1ee5319874dcee7eea78d6ec84e4eab7
Stephan Lipp <sphlpp91@gmail.com>
2023-10-05 22:03:49 +0200

    feat(fuzzing): add fuzzing mode to stats file

--- sast-fuzz/fuzzing/src/main.c
+++ sast-fuzz/fuzzing/src/main.c
@@ -6223,8 +6223,9 @@ EXP_ST u8 common_fuzz_stuff(char **argv, u8 *out_buf, u32 len) {
             }
         }
 
-        fprintf(stats_fd, "%d,%llu,%u,%u,%.2f,%d,%d,%d,%llu\n", fuzz_dur, cycle_count, init_cycle_interval,
-                cycle_interval, hc_reduct_factor, n_tbbs, n_tbbs_hit, n_tbbs_finished, unique_crashes);
+        fprintf(stats_fd, "%d,%llu,%s,%u,%u,%.2f,%d,%d,%d,%llu\n", fuzz_dur, cycle_count,
+                ((explore_status) ? "cov" : "dir"), init_cycle_interval, cycle_interval, hc_reduct_factor, n_tbbs,
+                n_tbbs_hit, n_tbbs_finished, unique_crashes);
 
         // Enforce file write operation ...
         fflush(stats_fd);
@@ -10803,9 +10804,9 @@ int main(int argc, char **argv) {
         FATAL("Could not create the SASTFuzz stats file!");
     }
 
-    fprintf(stats_fd, "%s,%s,%s,%s,%s,%s,%s,%s,%s\n", "fuzz_duration", "cycle_count", "init_cycle_length",
-            "cycle_interval", "hc_reduct_factor", "n_target_bbs", "n_target_bbs_hit", "n_target_bbs_finished",
-            "n_unique_crashes");
+    fprintf(stats_fd, "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", "fuzzing_dur", "cycle_count", "fuzzing_mode",
+            "init_cycle_length", "cycle_interval", "hc_reduct_factor", "n_target_bbs", "n_target_bbs_hit",
+            "n_target_bbs_finished", "n_unique_crashes");
 
     ck_free(stats_fname);
 #endif
