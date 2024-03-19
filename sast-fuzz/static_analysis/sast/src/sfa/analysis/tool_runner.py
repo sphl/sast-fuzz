39fb5b60ab64032d548a2c4453c278658a9fc0f6
Keno Hassler <40292329+kenohassler@users.noreply.github.com>
2024-03-19 09:55:43 +0000

    some infer paths are relative?

--- sast-fuzz/static_analysis/sast/src/sfa/analysis/tool_runner.py
+++ sast-fuzz/static_analysis/sast/src/sfa/analysis/tool_runner.py
@@ -287,10 +287,11 @@ class InferRunner(SASTToolRunner):
             vuln = flag["bug_type"]
 
             # remove the temp prefix from the path, but keep the rest
-            fparts = Path(file).parts
-            assert fparts[0] == "/" and fparts[1] == "tmp"
-            tmp_source = Path(fparts[0] + fparts[1]) / fparts[2] / fparts[3]
-            file = str(Path(file).relative_to(tmp_source))
+            if Path(file).is_absolute():
+                fparts = Path(file).parts
+                assert fparts[0] == "/" and fparts[1] == "tmp", f"file not in /tmp: {file}"
+                tmp_source = Path(fparts[0] + fparts[1]) / fparts[2] / fparts[3]
+                file = str(Path(file).relative_to(tmp_source))
 
             flags.add(SASTFlag(tool, file, line, vuln))
 
